f9a4461299a29619049f20d7d8d1d00f4affb2e9
Nachiket Salvi <nsalvi@mobiquityinc.com>
2018-12-20 19:11:31 +0530

    Added Side Drawer

--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -4,7 +4,8 @@ import Icon from 'react-native-vector-icons/Ionicons';
 const startTabs = () => {
   Promise.all([
     Icon.getImageSource('ios-map', 28),
-    Icon.getImageSource('ios-share-alt', 28)
+    Icon.getImageSource('ios-share-alt', 28),
+    Icon.getImageSource('ios-list', 28)
   ]).then(sources => {
     Navigation.startTabBasedApp({
       tabs: [
@@ -12,15 +13,38 @@ const startTabs = () => {
           screen: 'awesome-places.FindPlaceScreen',
           label: 'Find Place',
           title: 'Find Place',
-          icon: sources[0]
+          icon: sources[0],
+          navigatorButtons: {
+            leftButtons: [
+              {
+                id: 'sideDrawerToggle',
+                icon: sources[2],
+                title: 'Menu'
+              }
+            ]
+          }
         },
         {
           screen: 'awesome-places.SharePlaceScreen',
           label: 'Share Place',
           title: 'Share Place',
-          icon: sources[1]
+          icon: sources[1],
+          navigatorButtons: {
+            leftButtons: [
+              {
+                id: 'sideDrawerToggle',
+                icon: sources[2],
+                title: 'Menu'
+              }
+            ]
+          }
         }
-      ]
+      ],
+      drawer: {
+        left: {
+          screen: 'awesome-places.SideDrawer'
+        }
+      }
     });
   });
 };
