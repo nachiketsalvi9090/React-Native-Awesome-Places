eaa6abca2b3e7e213cae796907bced99a7d08ebd
Nachiket Salvi <nsalvi@mobiquityinc.com>
2018-12-24 15:35:55 +0530

    Created Custom button component

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import { View, Button, StyleSheet, ImageBackground } from 'react-native';
 // Import Assets
-import BackgroundImage from '../../assets/background.jpg'
+import BackgroundImage from '../../assets/background.jpg';
 // Import Component
 import startTabs from '../MainTabs/startMainTabs';
 import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
 import HeadingText from '../../components/UI/HeadingText/HeadingText';
 import MainText from '../../components/UI/MainText/MainText';
+import ButtonWithBackground from '../../components/UI/ButtonWithBackground/ButtonWithBackground';
 class AuthScreen extends Component {
   loginHandler = () => {
     startTabs();
+  };
+  onSwitchToLogin = () => {
+    alert('onSwitchToLogin');
   };
   render() {
     return (
@@ -18,13 +22,19 @@ class AuthScreen extends Component {
           <MainText>
             <HeadingText headingText="Please Login" />
           </MainText>
-          <Button title="Switch to login" />
+          <ButtonWithBackground color="#29aaf4" onPress={()=>{
+            alert("Switch To Login Pressed")
+          }}>
+            Switch To Login
+          </ButtonWithBackground>
           <View style={styles.inputContainer}>
             <DefaultInput placeholder="E-mail" style={styles.input} />
             <DefaultInput placeholder="Password" style={styles.input} />
             <DefaultInput placeholder="Confirm Password" style={styles.input} />
           </View>
-          <Button title="Login" onPress={this.loginHandler} />
+          <ButtonWithBackground color="#29aaf4" onPress={this.loginHandler}>
+            Switch To Login
+          </ButtonWithBackground>
         </View>
       </ImageBackground>
     );
@@ -37,9 +47,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
-  backgroundImage:{
-    width:"100%",
-    flex:1
+  backgroundImage: {
+    width: '100%',
+    flex: 1
   },
   inputContainer: {
     width: '80%'
