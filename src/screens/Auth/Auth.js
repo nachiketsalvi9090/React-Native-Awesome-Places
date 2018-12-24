8eef38feb95784963fd282a57d343b83a6b45d55
Nachiket Salvi <nsalvi@mobiquityinc.com>
2018-12-24 12:40:21 +0530

    Created  3 Text input on auth page and give styles to them

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
-import { View, Text,Button } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 import startTabs from '../MainTabs/startMainTabs';
 class AuthScreen extends Component {
-    loginHandler = () => {
-     startTabs();
-    };
-    render () {
-        return (
-            <View>
-                <Text>Auth Screen</Text>
-                <Button title="Login" onPress={this.loginHandler}></Button>
-            </View>
-        );
-    }
+  loginHandler = () => {
+    startTabs();
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Please Log In</Text>
+        <Button title="Switch to login" />
+        <View style={styles.inputContainer}>
+          <TextInput placeholder="E-mail" style={styles.input} underlineColorAndroid="transparent" />
+          <TextInput placeholder="Password" style={styles.input} underlineColorAndroid="transparent" />
+          <TextInput placeholder="Confirm Password" style={styles.input} underlineColorAndroid="transparent"/>
+        </View>
+        <Button title="Login" onPress={this.loginHandler} />
+      </View>
+    );
+  }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  inputContainer: {
+    width: '80%'
+  },
+  input: {
+    width: '100%',
+    borderWidth: 1,
+    borderColor: '#eee',
+    padding: 5,
+    margin: 8
+  }
+});
 export default AuthScreen;
