53fde70e4dc1550ca0b197d27c70f4734d725300
Nachiket Salvi <nsalvi@mobiquityinc.com>
2018-12-27 12:02:39 +0530

    Added Logout option in side Drawer

--- src/screens/SideDrawer/SideSrawer.js
+++ src/screens/SideDrawer/SideSrawer.js
@@ -1,26 +1,50 @@
 import React, { Component } from 'react';
-
-import { View, Text, Dimensions, StyleSheet } from 'react-native';
-
+import {
+  View,
+  Text,
+  Dimensions,
+  TouchableOpacity,
+  StyleSheet
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 class SideDrawer extends Component {
   render() {
     return (
       <View
         style={[
           styles.container,
-          { width: Dimensions.get('window').width * 0.5 }
+          { width: Dimensions.get("window").width * 0.8 }
         ]}
       >
-        <Text>Side Drawer</Text>
+        <TouchableOpacity>
+          <View style={styles.drawerItem}>
+            <Icon
+              style={styles.drawerItemIcon}
+              size={30}
+              name="md-log-out"
+              color="#aaa"
+            />
+            <Text>Sign Out</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 22,
+    paddingTop: 50,
     backgroundColor: 'white',
     flex: 1
+  },
+  drawerItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    backgroundColor: '#eee'
+  },
+  drawerItemIcon: {
+    marginRight: 10
   }
 });
 export default SideDrawer;
